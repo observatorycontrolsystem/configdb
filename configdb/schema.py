9e529781dcf85d935ca7ab619cb3b1b6033a322f
Matt Daily <mdaily@lco.global>
2021-06-28 17:25:15 -0700

    Force a full checkout for doc generation and building docker image.
    By default github actions checkout shallow clones a repo, which does not include much of the git metadata that setuptools-scm needs to generate version numbers and work correctly. Also add a better description for the head of the API docs.

--- configdb/schema.py
+++ configdb/schema.py
@@ -13,7 +13,11 @@ class ConfigDBSchemaGenerator(SchemaGenerator):
     def get_schema(self, *args, **kwargs):
         schema = super().get_schema(*args, **kwargs)
         schema['info']['title'] = 'Configuration Database'
-        schema['info']['description'] = 'An application with a database that stores observatory configuration'
+        schema['info']['description'] = 'Observatory Configuration Database is a simple frontend to a relational ' \
+                                        'database where we attempt to represent the physical state of a Telescope Network. It provides a RESTful API as well as HTML views of the data. ' \
+                                        'This is used by other applications in the observatory control system to understand what components make up the observatory, ' \
+                                        'and to allow for automated validation of component properties.' \
+                                        'This API documentation outlines the RESTful access, creation and modification of the components of the Configuration Database.'
         # Set the version to the latest git tag, and do not append any local information to the version number
         schema['info']['version'] = get_version(version_scheme=version_scheme, local_scheme='no-local-version')
         return schema
